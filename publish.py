3c7bcc157befaf158162f50d0abb00ee32613e30
Martin Wozniewicz <martin.woz@gmail.com>
2014-05-19 15:13:21 -0500

    First version of ftp deploy script, doesn't work with new directories

---
+++ publish.py
@@ -0,0 +1,34 @@
+from getpass import getpass
+from ftplib import FTP
+import os
+
+def publish():
+  username = raw_input("Username: ")
+  password = getpass()
+  ftp = FTP('mpwoz.com')
+  try: 
+    status = ftp.login(username, password)
+  except: 
+    print "Invalid login, try again."
+    return publish()
+  sendFiles(ftp)
+  ftp.quit()
+  print "Done."
+
+def sendFiles(ftp):
+  for root, dirs, files in os.walk('build'):
+    for fname in files:
+      full_fname = os.path.join(root, fname)
+      unprefixed_fname =  '/'.join(full_fname.split('/')[1:] ) # remove 'build' prefix
+      #print os.path.split(full_fname)
+      print(full_fname, unprefixed_fname)
+      ftp.dir()
+      ftp.mkd(root)
+      ftp.storbinary("STOR {0}".format(unprefixed_fname), open(full_fname, 'rb'))
+
+    
+
+
+if __name__ == "__main__":
+  publish()
+  
